e9c98af9f8ddb5fbbf6396632b38c1cb27037e7b
John Daniels <john.daniels@itison.com>
2014-06-06 14:24:29 +0100

    Version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "systems@secondmarket.com"
 license          "Apache 2.0"
 description      "Install HipChat handler for Chef runs"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.2"
+version          "1.0.3"
 
 depends          "chef_handler"
 
